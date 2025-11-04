3fe863498afa8bb0366587fe3034a377f2eba2a6
Ashsmith Khayrul <ash@solarhosting.cc>
2025-11-03 23:49:44 -0800

    added example code

--- day24.py
+++ day24.py
@@ -1,3 +1,23 @@
+def add_shipping(order_amt: float) -> float:
+    """ adds 4 for orders <= 10 (but non-zero), 8 for orders < 30, 12 for larger orders """
+    if order_amt == 0:
+        return 0
+    elif order_amt <= 10:
+        return order_amt + 4
+    elif order_amt < 30:
+        return order_amt + 8
+    else:
+        return order_amt + 12
+    
+
+def gadget_cost(num_gadgets: int, label: str) -> float:
+    """ computes cost, at $0.50 per gadget plus $0.05 per character in label """
+    return num_gadgets * (0.50 + (len(label) * 0.05))
+
+def test_gadget_cost_small() -> None:
+    """ test gadget_cost for small """
+    assert gadget_cost(1, "hi") == pytest.approx(0.6)
+
 def greet(name: str) -> str:
     """ produces Hello, name! """
     return "Hello, " + name + "!"
